8dd924ce0bb141a9f0d705f61210ca0b035bbba1
Vitaliy Vanchak <vvanchak2@gmail.com>
2019-03-04 11:11:03 +0200

    Changed core of server part and fixed tsconfig.json

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,71 @@
 import * as SerialPort from 'serialport';
+import * as socketIO from 'socket.io';
+import { Server } from 'https';
+
 
 export class ArduinoWebPort {
-  port: SerialPort;
-  parser: SerialPort.parsers.Readline;
-  portOpened = true;
+  socket: SocketIO.Server;
 
-  constructor(path: string, options?:SerialPort.OpenOptions) {
-    this.port = new SerialPort(path, options);
+  constructor (server: Server) {
+    this.socket = socketIO(server);
+  }
+
+  newBoard(path:string, portOptions: SerialPort.OpenOptions) {
+    return new Board(this.socket, path, portOptions);
+  }
+}
+
+class Board {
+  private socket: SocketIO.Server;
+  private port: SerialPort;
+  private parser: SerialPort.parsers.Readline;
+
+  constructor (
+    socket: SocketIO.Server, 
+    path:string, 
+    portOptions: SerialPort.OpenOptions
+  ) {
+    this.socket = socket;
+    this.port = new SerialPort(path, portOptions);
     this.parser = this.port.pipe(new SerialPort.parsers.Readline({ delimiter: '\n' }));
-    this.port.on('open', data => {
-      this.portOpened = true;
-      console.log('serial port ' + path + ' opened');
+    this.port.on('open', () => {
+      console.log('AWP: serial port '+ path +' opened');
     });
   }
 }
+
+// export class ArduinoWebPort {
+//   private port: SerialPort;
+//   private parser: SerialPort.parsers.Readline;
+//   private socket;
+//   private events = {};
+
+//   constructor(serialPort: SerialPort, socket) {
+//     this.port = serialPort;
+//     this.parser = this.port.pipe(new SerialPort.parsers.Readline({ delimiter: '\n' }));
+//     this.port.on('open', data => {
+//       console.log('serial port opened');
+//     });
+
+//   }
+
+//   destructor() {
+//     try {
+//       //TODO: map close all listeners
+//       this.port.close();
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+
+//   newArduinoListener(name: string, listenable: boolean, func?: Function) {
+//     if (this.events.hasOwnProperty(name)) {
+//       console.log(name + ' already exists, newArduinoEvent() stoped');
+//     } else {
+//       if (listenable) {
+
+//       }
+//       this.events[name] = this.parser.on('data', data => func(data));
+//     }
+//   }
+// }
