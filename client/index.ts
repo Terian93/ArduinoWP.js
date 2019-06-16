c6e2b4f1c20dd60ef119f9228eba7d26763d6728
Terian93 <vvanchak2@gmail.com>
2019-06-16 16:50:40 +0300

    fixed lint errors & patched Input and Output classes

--- client/index.ts
+++ client/index.ts
@@ -1,18 +1,23 @@
 import IO from 'socket.io-client';
-//const socketIO = require('socket.io-client');
-import * as socketIO from 'socket.io-client';
+
 class ArduinoWebPort {
-  private socket: SocketIOClient.Socket;
+  private _socket: SocketIOClient.Socket;
+  private conectionCallback?: (isDeployed: boolean) => void;
+
   constructor(path: string) {
-    //create socket
-    this.socket =IO.connect('http://localhost:3000'); //path
+    this._socket =IO.connect(path); //path
+  }
+
+  get socket() {
+    return this._socket;
   }
 
   createInput(event:any,debugMode =false){ 
-    return new Input(event, this.socket,debugMode);
+    return new Input(event, this._socket,debugMode);
   }
+
   createOutput(event: any,debugMode=false){
-    return new Output(event, this.socket,debugMode);
+    return new Output(event, this._socket,debugMode);
   }
 }
 
@@ -25,6 +30,7 @@ class Input {
   private trigger: (data: any) => boolean;
   private socket: SocketIOClient.Socket;
   private value: string;
+  private isDeployed: boolean;
 
   constructor(event: string, socket: SocketIOClient.Socket, isDebugModeActive:boolean ) {
     this.event = event;
@@ -33,12 +39,21 @@ class Input {
     this.socket = socket;
     this.debugMode = isDebugModeActive;
     this.fullListener = (data:any) => {
-      if(this.trigger(data)) {
+      if (this.trigger(data)) {
         this.value = this.middleware(data);
-        this.callback(data);
+        this.callback(this.value);
+        if ( this.debugMode ) {
+          console.log("Input:"+this.event+" => " + data);
+        }
       }
     };
+    this.isDeployed = false;
   }
+
+  get currentValue() {
+    return this.value;
+  }
+
   toogleDebugMode(){
     this.debugMode = !this.debugMode;
     return this;
@@ -46,66 +61,104 @@ class Input {
 
   addMiddleware(middleware: (data: any) => any) {
     this.middleware = middleware;
-    if(this.debugMode){
-    console.log("Input:"+this.event+" addMidlleware()");
+    if (this.isDeployed) {
+      console.warn('Changes would apply after call remove().deploy()');
     }
     return this;
   }
+
   changeMiddleware(middleware: (data: any) => any) {
     return this.addMiddleware(middleware);
   }
+
   removeMiddleware() {
     this.middleware = (data:any) => data;
-    if ( this.debugMode ) {
-
-      console.log("Input:"+this.event+" removeMidlleware()");
+    if (this.isDeployed) {
+      console.warn('Changes would apply after call remove().deploy()');
     }
+    return this;
   }
 
   addCallback(callback: (data: any) => any) {
     this.callback=callback;
+    if (this.isDeployed) {
+      console.warn('Changes would apply after call remove().deploy()');
+    }
     return this;
   }
-  changeCallBack(callback: (data: any) => any, index: number) {
+
+  changeCallBack(callback: (data: any) => any) {
     return this.addCallback(callback);
   }
-  removeCallback(callback: (data: any) => any, index: number) {
-    this.callback = (data:any)=>data;
-    if(this.debugMode) {
-      //.........
+
+  removeCallback() {
+    this.callback = (data:any) => data;
+    if (this.isDeployed) {
+      console.warn('Changes would apply after call remove().deploy()');
     }
+    return this;
   }
 
   addTrigger(trigger: (data:any) => any ){
-    this.trigger=trigger;
+    this.trigger = trigger;
+    if (this.isDeployed) {
+      console.warn('Changes would apply after call remove().deploy()');
+    }
     return this;
   }
+
   changeTrigger(trigger: (data:any) => any ){
     return this.addTrigger(trigger);
   }
+
   removeTrigger(){
     this.trigger = () => true;
+    if (this.isDeployed) {
+      console.warn('Changes would apply after call remove().deploy()');
+    }
+    return this;
   }
 
   private createListener() {
-    this.remove();
+    if (this.isDeployed) {
+      this.remove();
+      console.warn('createListener() was called when Input was deployed. (remove() called to prevent problems)');
+    }
     this.fullListener = (data:any) => {
       if(this.trigger(data)) {
         this.value = this.middleware(data);
         this.callback(data);
+        if ( this.debugMode ) {
+          console.log("Input:" + this.event + " => " + data);
+        }
       }
     };
+    return this;
   }
 
   deploy() {
-    this.createListener();
-    this.socket.on('test', this.fullListener); //TODO fix data type
+    if (!this.isDeployed) {
+      this.createListener();
+      this.socket.on('test', this.fullListener);
+      this.isDeployed = true;
+    } else {
+      throw new Error('Input already deployed (call removeListener() before deploy)');
+    }
     return this;
   }
 
   remove() {
     this.socket.off('test',this.fullListener);
+    this.isDeployed = false;
     return this;
+  }
+
+  deployListener() {
+    return this.deploy();    
+  }
+
+  removeListener() {
+    return this.remove();
   }
   
 }
@@ -129,17 +182,57 @@ class Output {
 
 
   constructor(event: string, socket: SocketIOClient.Socket, isDebugModeActive :boolean) {
-      this.socket= socket;
-      this.debugMode=isDebugModeActive;
-      this.event=event;
-      this.value="";
-    }
+    this.socket = socket;
+    this.debugMode = isDebugModeActive;
+    this.event = event;
+    this.value = "";
+  }
+
+  get currentValue() {
+    return this.value;
+  }
+
+  toogleDebugMode(){
+    this.debugMode = !this.debugMode;
+    return this;
+  }
+
+  addMiddleware(middleware: (data: any) => any) {
+    this.middleware = middleware;
+    return this;
+  }
+
+  changeMiddleware(middleware: (data: any) => any) {
+    return this.addMiddleware(middleware);
+  }
+
+  removeMiddleware() {
+    this.middleware = (data:any) => data;
+    return this;
+  }
+
+  addCallback(callback: (data: any) => any) {
+    this.callback=callback;
+    return this;
+  }
+
+  changeCallBack(callback: (data: any) => any) {
+    return this.addCallback(callback);
+  }
+
+  removeCallback() {
+    this.callback = (data:any)=>data;
+    return this;
+  }
 
   //deploy for output
-  private emit(value: any) {
-    const processedValue = this.middleware(value);
-    this.socket.emit(this.event, processedValue);
-    this.callback(value);
+  private emit(data: any) {
+    this.value = this.middleware(data);
+    this.socket.emit(this.event, this.value);
+    this.callback(this.value);
+    if ( this.debugMode ) {
+      console.log("Output:" + this.event + " emited => " + this.value);
+    }
     return true;
   }
 
@@ -148,21 +241,22 @@ class Output {
   }
 }
 
-class DS3201 {
-  private precision: number;
-  //get set
-  public input: Input;
-  public output: Output;
+//TODO special classes for sensors
+// class DS3201 {
+//   private precision: number;
+//   //get set
+//   public input: Input;
+//   public output: Output;
   
-  constructor(parameters) {
-    this.precision = 0.5;
-    this.input = new Input(...);
-    this.output = new Output(...);
-  }
+//   constructor(parameters) {
+//     this.precision = 0.5;
+//     this.input = new Input(...);
+//     this.output = new Output(...);
+//   }
 
-}
+// }
 
 //example
-const t = new DS3201();
-t.input.addCallback()
-module.exports = {Input, Output, DS3201, ...}
+// const t = new DS3201();
+// t.input.addCallback()
+// module.exports = {Input, Output, DS3201, ...}
